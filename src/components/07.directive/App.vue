a386bbe1d27d0d8c1bcca53495a13c5247b70786
Wendy <lienyun.chang@gmail.com>
2022-09-19 14:07:32 +0800

    custom directives(局部)

---
+++ src/components/07.directive/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>App</h1>
+    <hr>
+    <MyHome></MyHome>
+  </div>
+</template>
+
+<script>
+import MyHome from './Home.vue'
+
+export default {
+  name: "MyApp",
+
+  components: {
+    MyHome
+  }
+}
+</script>
---
+++ src/components/07.directive/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>Home</h3>
+    <hr>
+    <!-- 想一進入頁面input就被focus -->
+    <input type="text" class="form-control" v-focus>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "MyHome",
+  directives: {
+    focus: {
+      //當被綁定的元素插入到DOM中時，自動觸發mounted()
+      mounted(el) {
+        //el是原生DOM，指向那個input
+        el.focus()
+      }
+    }
+  }
+
+}
+</script>
